7d5dac94c5bfaca1411ebda5f0c8c0d0bb286811
Ben <benranson5@gmail.com>
2021-03-23 13:15:25 +0000

    added additional methods to the list section

--- lists_and_tuples.py
+++ lists_and_tuples.py
@@ -9,24 +9,27 @@ shopping_list = ['bread', 'milk', 'eggs', 'ham', 'apples']
 #    0        1       2      3       4
 print(shopping_list[0])
 
+# You can slice a list in the exact same way you would slice a string, this includes reverse slicing
+print(shopping_list[1:3])
+print(shopping_list[-2])
+
 
 # To replace something in a list you explicitly overwrite it
 print(shopping_list)
 shopping_list[0] = 'tomatoes'
 print(shopping_list)
 
-# To add something to a list you should use .append()
+# To add something to a list you should use .append() or .insert if using an index
 print(shopping_list)
 shopping_list.append('sausage')
+print(shopping_list)
+shopping_list.insert(0, "avocado")
 print(shopping_list)
 
 # To delete something from a list you should use .remove() or .pop() if using the index
 print(shopping_list)
 shopping_list.remove('eggs')
+print(shopping_list)
 shopping_list.pop(0)
 print(shopping_list)
 
-
-# You can slice a list in the exact same way you would slice a string, this includes reverse slicing
-print(shopping_list[1:3])
-print(shopping_list[-2])
